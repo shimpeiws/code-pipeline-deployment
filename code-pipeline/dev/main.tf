28d612940807a19e8f6cfa06d538f15d75b1f61e
Shimpei Takamatsu <shimpeiws@gmail.com>
2020-09-28 13:24:42 +0900

    Pipeline settings

--- code-pipeline/dev/main.tf
+++ code-pipeline/dev/main.tf
@@ -12,6 +12,15 @@ terraform {
 }
 
 module "code-pipeline" {
-  source   = "../modules/code-pipeline"
-  env_name = "dev"
+  source                     = "../modules/code-pipeline"
+  env_name                   = "dev"
+  code_build_project_name    = "code-pipeline-deployment"
+  code_pipeline_project_name = "code-pipeline-deployment"
+  github_project_url         = "https://github.com/shimpeiws/code-pipeline-deployment"
+  github_account_name        = "shimpeiws"
+  github_oauth_token         = var.github_oauth_token
+  github_repo_name           = "code-pipeline-deployment"
+  github_branch_name         = "master"
+  plan_buildspec_path        = "./code-build/terraform-plan-build-spec.yml"
+  apply_buildspec_path       = "./code-build/terraform-apply-build-spec.yml"
 }
--- code-pipeline/modules/code-pipeline/main.tf
+++ code-pipeline/modules/code-pipeline/main.tf
@@ -1,4 +1,4 @@
-data "aws_iam_policy_document" "assume_role_policy" {
+data "aws_iam_policy_document" "code_build_assume_role_policy" {
   statement {
     effect  = "Allow"
     actions = ["sts:AssumeRole"]
@@ -11,7 +11,7 @@ data "aws_iam_policy_document" "assume_role_policy" {
 
 resource "aws_iam_role" "code_build_role" {
   name               = "code-build-role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+  assume_role_policy = data.aws_iam_policy_document.code_build_assume_role_policy.json
 }
 
 data "aws_iam_policy_document" "code_build_policy" {
@@ -24,6 +24,17 @@ data "aws_iam_policy_document" "code_build_policy" {
     ]
     resources = ["*"]
   }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:GetBucketVersioning",
+      "s3:PutObject"
+    ]
+    resources = ["*"]
+  }
 }
 
 resource "aws_iam_role_policy" "code_build_policy" {
@@ -31,11 +42,11 @@ resource "aws_iam_role_policy" "code_build_policy" {
   policy = data.aws_iam_policy_document.code_build_policy.json
 }
 
-resource "aws_codebuild_project" "deploy" {
-  name          = "${var.code_build_project_name}"
-  description   = "code build for plan"
+resource "aws_codebuild_project" "plan" {
+  name          = "${var.code_build_project_name}-terraform-plan"
+  description   = "code build for terraform plan"
   build_timeout = "60"
-  service_role  = "${aws_iam_role.codebuild_role.arn}"
+  service_role  = aws_iam_role.code_build_role.arn
 
   artifacts {
     type = "NO_ARTIFACTS"
@@ -43,7 +54,7 @@ resource "aws_codebuild_project" "deploy" {
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "hashicorp/terraform:light"
+    image                       = "hashicorp/terraform:0.13.3"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
     privileged_mode             = true
@@ -51,8 +62,165 @@ resource "aws_codebuild_project" "deploy" {
 
   source {
     type            = "GITHUB"
-    location        = "${var.github_project_url}" //GitHub project URL
+    location        = var.github_project_url
     git_clone_depth = 1
-    buildspec       = "${var.buildspec_path}" //GitHubにあるbuildspec.ymlの場所
+    buildspec       = var.plan_buildspec_path
+  }
+}
+
+resource "aws_codebuild_project" "apply" {
+  name          = "${var.code_build_project_name}-terraform-apply"
+  description   = "code build for apply"
+  build_timeout = "60"
+  service_role  = aws_iam_role.code_build_role.arn
+
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "hashicorp/terraform:0.13.3"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+    privileged_mode             = true
+  }
+
+  source {
+    type            = "GITHUB"
+    location        = var.github_project_url
+    git_clone_depth = 1
+    buildspec       = var.apply_buildspec_path
+  }
+}
+
+data "aws_iam_policy_document" "codepipeline_assume_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      identifiers = ["codepipeline.amazonaws.com"]
+      type        = "Service"
+    }
+  }
+}
+
+resource "aws_iam_role" "codepipeline_role" {
+  name               = "codepipeline-role"
+  assume_role_policy = data.aws_iam_policy_document.codepipeline_assume_role.json
+}
+
+data "aws_iam_policy_document" "code_pipeline_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:GetBucketVersioning",
+      "s3:PutObject"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "codebuild:BatchGetBuilds",
+      "codebuild:StartBuild"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "code_pipeline_policy" {
+  role   = aws_iam_role.codepipeline_role.name
+  policy = data.aws_iam_policy_document.code_pipeline_policy.json
+}
+
+resource "aws_s3_bucket" "artifact" {
+  bucket = "${var.code_pipeline_project_name}-s3-artifact"
+  acl    = "private"
+}
+
+data "aws_kms_alias" "s3kmskey" {
+  name = "alias/aws/s3"
+}
+
+resource "aws_codepipeline" "without_approval" {
+  name     = "codepipeline-main"
+  role_arn = aws_iam_role.codepipeline_role.arn
+
+  artifact_store {
+    location = aws_s3_bucket.artifact.bucket
+    type     = "S3"
+
+    encryption_key {
+      id   = data.aws_kms_alias.s3kmskey.arn
+      type = "KMS"
+    }
+  }
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "ThirdParty"
+      provider         = "GitHub"
+      version          = "1"
+      output_artifacts = ["source_output"]
+
+      configuration = {
+        Owner      = var.github_account_name
+        OAuthToken = var.github_oauth_token
+        Repo       = var.github_repo_name
+        Branch     = var.github_branch_name
+      }
+    }
+  }
+
+  stage {
+    name = "Plan"
+
+    action {
+      name            = "Plan"
+      category        = "Build"
+      owner           = "AWS"
+      provider        = "CodeBuild"
+      input_artifacts = ["source_output"]
+      version         = "1"
+
+      configuration = {
+        ProjectName = aws_codebuild_project.plan.name
+      }
+    }
+  }
+
+  stage {
+    name = "Apply"
+
+    action {
+      name            = "Apply"
+      category        = "Build"
+      owner           = "AWS"
+      provider        = "CodeBuild"
+      input_artifacts = ["source_output"]
+      version         = "1"
+
+      configuration = {
+        ProjectName = aws_codebuild_project.apply.name
+      }
+    }
   }
 }
